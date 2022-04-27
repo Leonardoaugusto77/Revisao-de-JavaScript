fb9dcb9bed19d06e15f8b1f753d6137fb76bd0b3
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-04-27 08:38:42 -0300

    add
    add

---
+++ Aula 01/documenetwrite.js
@@ -0,0 +1,7 @@
+document.write("50" + 100, '<br>') // Utilizando aspas será impresso uma String, no casso desse trecho de código será feita uma concatenação
+
+document.write(50 + 100, '<br>') // Fora das aspas o Código vai entender que 50 + 100 é uma multiplicação 
+
+document.write("CFB Cursos <br> Curso de JavaScript!") // Por ser uma linguagem focada em Dev Web, o JavaScript aceita tags e estilios
+
+document.write("<h1 style='color: #f00'>O texto Vai ficar em Vermelho</h1>") // Exemplo de estilização utilizando JavaScript  
