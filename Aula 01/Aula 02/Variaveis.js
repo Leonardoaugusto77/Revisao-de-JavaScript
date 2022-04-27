3f315a912f6da7ad24ee2e549e2ecf9635d5f3c6
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-04-27 08:58:08 -0300

    add

---
+++ Aula 01/Aula 02/Variaveis.js
@@ -0,0 +1,17 @@
+var unica // Essa é uma variável com o Armazenamento unico
+var canal, nome, sites // Armazenamento de variáveis múltiplas, elas não tem o valor definido no momento
+var SemValor // Se dermos um console.log() ou document.write o valor retornado dessa variável é nulo
+var PJH = 'Project_Heisen'
+var PRO = 'Desenvolvedor Front - end Jr'
+
+var n1 = 2
+var n2 = 3
+var txt = 'O Resultado é '
+
+var res = txt + (n1 + n2) // Para conseguirmos concatenar Numbers e Strings será necessário utilizar () para o Navegador entender
+                          // que se trata de uma soma
+
+//console.log(PJH + ' ' +  PRO) // É possível fazer concatenação com as variáveis 
+
+
+console.log(res)
