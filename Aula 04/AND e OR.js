ecb7ed2a592b007ac51b14b1dce5af571abb78b6
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-05-06 10:00:40 -0300

    add
    add

---
+++ Aula 04/AND e OR.js
@@ -0,0 +1,41 @@
+/* AND (&&) e OR(||), são comparadores que trabalham com valores booleanos
+
+   AND (&&) Só retorna verdadeuri quando todos os testes forem True
+   
+   AND - Table
+   V + F = F
+   F + V = F
+   V + V = V
+   F + F = F
+   
+   
+
+   OR - (||), só retorna falso quando todos os testes forem falsos
+
+   OR - Table
+   V + F = V
+   F + V = V
+   V + V = V
+   F + F = F
+
+
+*/
+
+
+
+var pontos = 40
+// Vitoria => >=30 <=40 || >= 70 && <=80
+
+   // V                    V 
+if((pontos >=30 ) && (pontos <= 40) // = V 
+  
+  ||
+  // F                  V
+  (pontos>= 70) && (pontos) <=80){ // = F
+       // Para funcionar a comparação é importante usar parênteses uns dentro dos outros 
+
+
+       console.log("Você Ganhou!") // Para não ficar tudo em uma linha é interessante não deixar tudo em uma linha, mas isso depende do tamanho da comparação que você que fazer!                        
+} else{
+    console.log("Você perdeu!")
+}
