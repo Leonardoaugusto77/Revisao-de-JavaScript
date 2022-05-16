f0bc55dc92e68b38dc99eb315eae23d6918dea0c
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-05-16 07:58:03 -0300

    add
    add

---
+++ Aula 04/BDC.js
@@ -0,0 +1,27 @@
+/** Bloco de comandos (BDC) {} <- tudo que estiver dentro das chaves vai ser considerado pelo bloco de comando, ela
+ * não é necessária em todos os casos, porém é interessante utilizar para uma organização melhor do código veja os exemplos abaixo
+ */
+
+
+//Exemplo utilizando as chaves
+
+var num = 10
+
+if(num > 10){
+    console.log("Maior que " + num)
+} else {
+    console.log("Menor que " + num)
+}
+
+
+// Sem as chaves
+
+var num = 10
+
+if(num > 10)
+    console.log("Maior que 10" )
+ 
+else 
+    console.log("Menor que 10")
+
+// Quando existem mais instruções é obrigatório usar chaves {}
