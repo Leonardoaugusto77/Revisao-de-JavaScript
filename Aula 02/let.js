d3672769d861b4c837f33166c1721e44c5f188f1
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-05-26 09:17:04 -0300

    add
    add

---
+++ Aula 02/let.js
@@ -0,0 +1,15 @@
+/* Criando Vars com Let
+    let é mais utilizado para criar variaveis com o escopo
+    dentro de condições como if, while, for e etc... 
+
+
+*/
+
+var num = 10
+
+if(num==10){
+    let num=5 // <-- No momento que usamos let declaramos que a var num tem o escopo limitado ao bloco que ela se encontra
+    console.log(num) // <-- O resultado será 5
+}
+
+console.log(num) // <-- esse num está puxando a Var num que está fora do escopo do if então o resultado será 10
