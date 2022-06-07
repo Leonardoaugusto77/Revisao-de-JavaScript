7307720b7af2ab94f4d9de77a2e96b470bf3749c
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-06-07 08:56:44 -0300

    Math bib

--- Aula 07/math.js
+++ Aula 07/math.js
@@ -38,3 +38,46 @@ console.log(Math.round(n3)) // o Math.round() se for 10.5 ele vai aredondar o va
 var n4 = 4
 
 console.log(Math.pow(n4,2)) // Eleva um número a outro
+
+var n5 = 5
+
+console.log(Math.floor(n5)) // Vai aredondar o valor para baixo
+
+
+var n6 = Math.random() // Retorna números aleatorios entre 0 e 1 
+
+console.log(n6)
+
+
+// Podemos utilizar dois metodos ao mesmo tempo por exemplo:
+
+var n7 = 10
+
+var res = Math.round(Math.random() * n7)
+
+console.log(res)
+
+
+
+// Exemplos utilizando o FOR
+
+
+var n8 = 60
+
+for(let i = 0; i < 6; i++){
+    console.log(Math.round(Math.random() * n8))
+}
+
+// Logo em breve vamos estudar a fundo angulos no JavaScript por enquanto só acompanhe a aula 
+
+var angulo = 45
+
+var c = Math.cos(angulo * Math.PI / 180)
+var s = Math.sin(angulo * Math.PI/180)
+
+console.log("Angulo:" + angulo + " Cosseno " + c + " Seno " + s)
+
+
+var n9 = Math.max(1,2,3,4,5,6,7,8,9) // Retorna o número maximo, eo min o menor valor existente
+
+console.log(n9)
