0ca02a26722fd726986180c796ad51bd80d13777
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-06-21 22:09:48 -0300

    Update JavaScript

--- Aula 06/INeDEC.js
+++ Aula 06/INeDEC.js
@@ -2,7 +2,7 @@
 
 var num = 10
 
-num=num+1 // Incremento Simples
+num = num + 1 // Incremento Simples
 // num=num-1 Decremento Simples
 
 /* A melhor maneira de fazer esse tipo incremento e utilizando num++ ou para subtrair --
@@ -13,12 +13,27 @@ num=num+1 // Incremento Simples
 
     Pos Incremento <-- Adiciona no final
     Pre Incremento <-- Adiciona no inicio, utilizado em casos que o Pos não é lido de imediato
-
-
-
-
 */
 
 console.log(num)
 
 
+var nums = 10
+var x
+
+x=nums++
+
+console.log(x)
+
+/* 
+    Embora incrementemos o valor de num e passamos para a variável x, ao imprimir não
+    será retornado o valor 11 e sim o valor 10. Isso pq no pós incremento o valor é adicionado á variável depois.
+    
+    Nesse caso a cima primeiro e adicionado o valor de num a x e só depois num tem o valor incrementado!
+    Para retornarmos o 11 será necessário incrementar primeiro e depois passar a Var desse jeito.
+   
+    x=++num
+*/
+
+
+  
