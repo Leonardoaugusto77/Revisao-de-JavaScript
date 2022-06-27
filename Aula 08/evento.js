be592ac1cbb95773500aebe17ac9022c05d82c64
Leonardo Augusto Dos santos <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-06-27 17:19:58 -0300

    add
    add

--- Aula 08/evento.js
+++ Aula 08/evento.js
@@ -1,9 +1,10 @@
 // Trabalhando com Eventos no JavaScript
 
-
 /* 
    Eventos são ações ou ocorrência que acontecem no sistema que estamos desenvolvendo, no qual
    este te alerta sobre essas ações para que você possa responder de alguma forma.*/
+
+// Exemplo 01:
 
  function mudaCor(obj,cor){
     obj.style.backgroundColor=cor
@@ -11,6 +12,18 @@
  }
  
 mudaCor()
+
+
+/*
+    Na função a cima passamos dois parâmetros para a função mudaCor(), logo a baixo
+    o obj vai receber o style e backgroundColor como método e este recebera o parâmetro cor
+    como value.
+
+    Para entender a segunda parte do código leia o comentário no arquivo HTML
+*/
+
+
+// Exemplo - 02:
 
 function mudaPos(obj){
     obj.style.position="absolute";
@@ -20,15 +33,15 @@ function mudaPos(obj){
 
 
 function ola(){
-    let data = new Date()
-    var hora = data.getHours()
+    let data = new Date() // Pega os métodos do Date
+    var hora = data.getHours() // Pega as horas da máquina
     
     let txt = ""
     
     if(hora < 12){
         txt = "Bom dia!"
     } else if(hora < 18){
-        txt = "Boa tarde!"
+        txt = "Boa tarde!" 
     } else {
         txt = "Boa noite"
     }
