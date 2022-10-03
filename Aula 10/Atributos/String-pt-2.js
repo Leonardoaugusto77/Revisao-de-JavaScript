5056638d4440ffddefe5eebd73f13a201c600c11
Leonardo Augusto <84159648+Leonardoaugusto77@users.noreply.github.com>
2022-10-03 14:52:34 -0300

    updates

--- Aula 10/Atributos/String-pt-2.js
+++ Aula 10/Atributos/String-pt-2.js
@@ -1,3 +1,4 @@
+
 /* Trabalhando com Match parte - 02 */
 
 
@@ -40,7 +41,8 @@ document.write("<br>")
 /* Método charAt
 
 charAt - Retorna o caracter que eu indiquei a posição como paramentro do metodo
-
+charCodeAt - retorna um número inteiro entre 0 e 65535 que representa a unidade de código UTF-16 no índice fornecido.
+fromCharCode - Transforma o caracter em código
 
 */
 
@@ -48,7 +50,8 @@ var texto = document.getElementById("txt").innerHTML
 
 
 var res = texto.charAt(2)
-var res2 = texto.charCodeAt(2)
+var res2 = texto.charCodeAt(2) // Retorna o codigo da string indicada pela posição
+
 
 
 document.write(res)
@@ -57,6 +60,24 @@ document.write("<br>")
 
 document.write(res2)
 
+
+document.write(String.fromCharCode(67))
+
+
+
+/* Método concat
+
+    Junta uma string na outra, é possível juntar 3 ou mais strings
+
+*/
+
+var texto = document.getElementById("txt").innerHTML
+var text02 = " - aula 46 do curso de javascripts"
+
+
+var res = texto.concat(text02)
+
+document.write(res)
 
 
 
