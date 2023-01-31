df7d7bc4444c12852a83254106e1845be52364e8
Heisen <leonardopjh30@gmail.com>
2023-01-31 08:42:35 -0300

    up

--- Aula 14/hasFocus.js
+++ Aula 14/hasFocus.js
@@ -1,1 +1,15 @@
 /* hasFocus -> Retorna true o false se uma página está em foco ou não */
+
+var dvmsg = document.querySelector('#dvmsg')
+var simnao = document.querySelector('#simnao')
+
+const foco = () => {
+    if(document.hasFocus()){
+        document.body.setAttribute('class', 'dia')
+    } else {
+        document.body.setAttribute('class', 'noite')
+    }
+}
+
+
+setInterval(foco, 1000)
