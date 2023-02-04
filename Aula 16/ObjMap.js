8da7cb4091969c3d16e4b44adbda81ff7658134b
Heisen <leonardopjh30@gmail.com>
2023-02-04 14:41:20 -0300

    upadates-aulas

--- Aula 16/ObjMap.js
+++ Aula 16/ObjMap.js
@@ -1,16 +1,42 @@
 /* Objeto map -> É uma coleção simples de par (key, value) podemos usar tipo primitivos os objetos */
 
-let produtos = new Map()
-let num = ['01','02','03','04','05']
+let produtos = new Map([
+    ['01', 'mouse'],
+    ['02', 'teclado'],
+    ['03', 'Headseat'],
+    ['04', 'Monitor'],
+])
 
-produtos.set('01', 'Mouse')
-produtos.set('02', 'Teclado')
-produtos.set('03', 'Monitor')
-produtos.set('04', 'Headseat')
 
-console.log(produtos.size) // Me devolve o tamanho do map
+// console.log(produtos.size) // Me devolve o tamanho do map
 
-/* Para definir elementos usamos o set e para pegar os elementos o get*/
+// produtos.clear // Limpa dos os itens que estão no map
+// produtos.delete('03') // Deleta o item que foi passado de dentro do map
 
-console.log(produtos.get('02'))
+// forEach() -> Permite executar uma função que é passada como callback para cada um dos elementos do nosso map
+
+// Retorna um objeto do tipo interator que redefinir a chave do map
+
+console.log(produtos.has('02')) // Procura o elemento na lista 
+
+let it = produtos.keys() // valor da chave
+let itv = produtos.values() // valor do value
+
+console.log(it.next())
+console.log(itv.next().value)
+
+const info = (chave, valor) => {
+    console.log('Chave ' + chave + ' - Valor ' + valor)
+}
+
+produtos.forEach(info)
+
+
+
+/* console.log(produtos.get('02')) para definir elementos usamos o set e para pegar os elementos o get*/
+
+
+for(p of produtos){
+    console.log(p[1][1])
+}
 
